71e07e62c395211a84a1b61ca72ad7c59f565665
Davi Lorini Junior <98284261+daviljr@users.noreply.github.com>
2025-11-21 21:20:57 -0300

    Add finance module with budgeting tips

---
+++ modules/finance.js
@@ -0,0 +1,5 @@
+function run(__params){
+  const tip='Anote receitas e despesas. Encontre 1 gasto para reduzir.';
+  const micro={task:'Ofereça um serviço online ou revise preço de venda.'};
+  return {ok:true,tip,micro};
+}
